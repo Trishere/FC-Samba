386d80e410bbc195652b9a50de0f18d1b5ac15b5
Trishere <Trinhductritri123@gmail.com>
2026-07-30 15:09:34 +0700

    cap nhat website

--- halloffame.js
+++ halloffame.js
@@ -17,6 +17,12 @@ const hallOfFamePeriods = [
         year: 2027,
         months: [1, 2, 3]
     },
+    {
+        id: "2027-q2",
+        title: "Apr - Jun 2027",
+        year: 2027,
+        months: [4, 5, 6]
+    },
 
 
 
